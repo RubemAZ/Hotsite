ba65b1910b1996bd070bb5a6a799f06cb806d0da
Rubem Azevedo Mazzetto <87815929+RubemAZ@users.noreply.github.com>
2025-04-22 06:41:18 -0300

    ADD new layout

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import MarketingPage from "./templates/marketing-page/MarketingPage";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <MarketingPage/>
+  );
+}
